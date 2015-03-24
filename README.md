9e67ae5fffed7b3f1902e6dc27614e77e067f4e4
Stephanie Vasquez-Soltero <StephieVictoria@gmail.com>
2015-03-24 03:11:41 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+JavaScript Coding Challenge Question 1 - The Awesome Index
+
+This is my solution to the JavaScript Development Accelerator coding challenge question 1.
+
+Challenge:
+Write a function that takes an array of data in the below format. Assume that this function is run in a browser. It should update a
+tag with id="awesome-index" with the average awesome-index of all programmers. Your solution should continue to work even if more people are added to the array. Write two versions, one using lodash (or underscore) and jQuery, and one using only Javascript utilities native to the browser. *
---
+++ script.js
@@ -0,0 +1,58 @@
+var people = [
+{ name: "Bob",
+occupation: "programmer",
+awesomeIndex: 7 },
+{ name: "Alice",
+occupation: "programmer",
+awesomeIndex: 9 },
+{ name: "Zaphod",
+occupation: "President of the Galaxy" } ];
+
+// Using Native JS to obtain the average awesome-index
+function sumAwesomeIndexNS(array) {
+  var sum = 0;
+  var numOfProgrammers = 0;
+
+  array.forEach(function(element, index) {
+    if (element.occupation === "programmer") {
+      numOfProgrammers++;
+      sum += element.awesomeIndex;
+    }
+  });
+ return sum / numOfProgrammers;
+}
+
+// Using JQuery and Underscore to obtain the average awesome-index
+function sumAwesomeIndexUJ(array) {
+  var memo = 0;
+  var numOfProgrammers = 0;
+
+  var theSumIndex = _.reduce(array, function(memo, programmers) {
+    if (programmers.occupation === "programmer") {
+      numOfProgrammers++;
+      return memo + programmers.awesomeIndex;
+    }
+
+  return memo;
+
+  }, 0);
+
+  return theSumIndex / numOfProgrammers;
+
+ }
+
+$(function() {
+  $("button").on("click", function() {
+
+  var clicked = $(this).attr("id");
+
+      var resText;
+      if (clicked === "Underscore/jQuery") {
+        resText = sumAwesomeIndexUJ(people);
+        $("#awesome-index").text("This result is due to " + clicked + ": " + resText);
+      } else {
+        resText = sumAwesomeIndexNS(people);
+        document.getElementById("awesome-index").innerHTML = "This result is due to Native Javascript: " + resText;
+      }
+    });
+});
